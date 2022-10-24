f8dd660c455715d2724d4262f51057ea4fdcc8c5
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2022-10-24 01:42:15 -0600

    added BrowserRoute

--- oliseh-frontend/src/App.tsx
+++ oliseh-frontend/src/App.tsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AboutPage } from './pages/AboutPage/AboutPage';
 import { HomePage } from './pages/HomePage/HomePage';
 
 const App = () => {
   return (
-    <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/about" element={<AboutPage/>}/>
-    </Routes>
+    <BrowserRouter> 
+      <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
